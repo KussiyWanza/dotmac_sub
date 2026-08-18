83451fd1bc6869416df1b96e70563e45fcf3fee7
michaelayoade <32591929+michaelayoade@users.noreply.github.com>
2026-08-18 12:46:11 +0100

    fix(inbox): share timeout fetcher with reopen loader

--- static/js/admin-inbox.js
+++ static/js/admin-inbox.js
@@ -42,6 +42,7 @@
       upstreamSignal?.removeEventListener("abort", abortFromUpstream);
     }
   };
+  window.inboxFetchWithTimeout = fetchWithTimeout;
 
   window.inboxTeamFilterBuilder = function inboxTeamFilterBuilder(initialJson) {
     const blankCondition = (bucket = "and") => ({
@@ -2926,7 +2927,7 @@
       sync();
     });
 
-    fetchWithTimeout(form.dataset.templateEndpoint, {
+    window.inboxFetchWithTimeout(form.dataset.templateEndpoint, {
       headers: { Accept: "application/json" },
     })
       .then((response) => response.json().then((payload) => ({ response, payload })))
